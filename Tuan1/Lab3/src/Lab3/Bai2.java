bef8bad3041062ec25cf004283f3bda4388414e1
lythehoc <55098037+lythehoc@users.noreply.github.com>
2019-09-11 00:17:52 +0700

    Hoan thanh bai1 bai2 bai3 lab3
    Chua hoan thanh bai4 lab3

---
+++ Tuan1/Lab3/src/Lab3/Bai2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab3;
+
+/**
+ *
+ * @author lythehoc
+ */
+public class Bai2 {
+    public static void main(String[] args) {
+        for(int i=1; i<=9; i++ ){
+            System.out.println("------------");
+            for(int j=1; j<=10 ; j++){
+            System.out.println(+i+" x "+j+" = "+i*j);
+            }
+        }    
+    }
+}
---
+++ Tuan1/Lab3/src/Lab3/Bai4.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab3;
+
+/**
+ *
+ * @author lythehoc
+ */
+public class Bai4 {
+    public static void main(String[] args) {
+        String ten;
+        Kho qua vay :(
+    }
+}
