3263a0b92a003ab5f6947893ba3165ee393f9ba9
lythehoc <55098037+lythehoc@users.noreply.github.com>
2019-09-10 22:05:03 +0700

    Hoan thanh Lab2
    Hoan thanh 4 bai cua Lab2

---
+++ Tuan1/Lab2/src/Lab2/Bai3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lythehoc
+ */
+public class Bai3 {
+    public static void main(String[] args) {
+        double a, tien;
+        Scanner hoc = new Scanner(System.in);
+        System.out.println("Nhap vao so dien su dung trong thang: ");
+        a = hoc.nextDouble();
+        
+        if (a < 50) {
+            tien = a*1000;
+            System.out.println("Tien dien phai dong la: "+tien +" dong");
+        }
+        else {
+            tien= 50*1000+(a-50)*1200;
+            System.out.println("Tien dien phai dong la: "+tien+" dong");
+        }
+    }
+}    
